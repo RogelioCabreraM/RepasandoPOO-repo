4ab4aa8a3cb749c9d543e8392f0853fe3a9433f9
roger cm <ingrogeliocabrera@gmail.com>
2026-06-04 20:46:53 -0600

    clases creadas

---
+++ src/Card.java
@@ -0,0 +1,13 @@
+public class Card {
+    private int number;
+    private String letter;
+
+    public Card(int number, String letter){
+        this.number = number;
+        this.letter = letter;
+    }
+
+    public void showCard(){
+        System.out.println(this.number + this.letter);
+    }
+}
